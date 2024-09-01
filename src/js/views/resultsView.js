57149f54bcc11464388f5049916c695ca7206a04
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-09-01 17:10:55 +0700

    add search module

---
+++ src/js/views/resultsView.js
@@ -0,0 +1,30 @@
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+import View from './View.js';
+class resultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again :)';
+  _message = '';
+  _generateMarkup() {
+    return this._data
+      .map(result => this._generateMarkupPreview(result))
+      .join('');
+  }
+  _generateMarkupPreview(result) {
+    return `
+        <li class="preview">
+            <a class="preview__link " href="#${result.id}">
+              <figure class="preview__fig">
+                <img src="${result.sourceUrl}" alt="Test" />
+              </figure>
+              <div class="preview__data">
+                <h4 class="preview__title">${result.title}</h4>
+                <p class="preview__publisher">${result.publisher}</p>
+
+              </div>
+            </a>
+          </li>
+    `;
+  }
+}
+
+export default new resultsView();
