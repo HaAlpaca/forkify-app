43954421f2941cc2b5feba1d1edc44f449eb4205
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-09-02 15:44:42 +0700

    add pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -10,8 +11,8 @@ import 'regenerator-runtime/runtime';
 
 ///////////////////////////////////////
 
-if(module.hot) {
-  module.hot.accept()
+if (module.hot) {
+  module.hot.accept();
 }
 
 const controlRecipe = async function () {
@@ -36,19 +37,30 @@ const controlSearchResults = async function () {
     const query = searchView.getQuery();
     if (!query) return;
     // find and clear input
+    resultsView.renderSpinner();
     await model.loadSearchResults(query);
-
-    resultsView.renderSpinner()
-    resultsView.render(model.state.search.results)
+    // render result
+    resultsView.render(model.getSearchResultsPage());
+    // render pagination
+    paginationView.render(model.state.search);
   } catch (error) {
     console.log(err);
   }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
 };
 
 // controlRecipe();
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
