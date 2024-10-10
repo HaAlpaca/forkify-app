8b8ebc7a1d49d0e3a1dc8f8351a88442e5e0abf4
HaAlpaca <dangminhhaworkspace@gmail.com>
2024-10-11 00:18:17 +0700

    fix render spinner when empty result

--- src/js/controller.js
+++ src/js/controller.js
@@ -44,19 +44,15 @@ const controlRecipes = async function () {
 };
 const controlSearchResults = async function () {
   try {
-    resultsView.renderSpinner();
+    // resultsView.renderSpinner();
 
-    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
-    // 2) Load search results
     await model.loadSearchResults(query);
 
-    // 3) Render results
     resultsView.render(model.getSearchResultsPage());
 
-    // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
